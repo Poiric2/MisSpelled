2b8ce08addfbc5428468fdebaf8f63a3e5931576
Pheonix <colecarpenter@gmail.com>
2018-02-21 17:28:55 -0500

    Added camera and player control scripts as well as a test scene used to make them

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 14f;
+    public float interactDistance = 5f;
+    RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float forward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(strafe, 0, forward);
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, interactDistance)){
+                if(hit.transform.tag == "interactable"){
+                    print("interact");
+                }
+            }
+
+        }
+
+	}
+}
