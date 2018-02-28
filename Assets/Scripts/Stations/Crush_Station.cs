35ac44bf41c5761a742522ce4d31a1cbe2e265e4
Claira_Poirier <poiric2@rpi.edu>
2018-02-28 14:43:51 -0500

    Added station actions to stations

---
+++ Assets/Scripts/Stations/Crush_Station.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crush_Station : Station {
+
+	// Use this for initialization
+	void Start () {
+        anchor = new Vector3(-3f, 2.4f, -.5f);
+        anchorRot = new Vector3(30f, 270f, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Crush(ref Ingredient ingredient) {
+
+		// Elemental Interactions
+		ingredient.red += ingredient.orange;
+		ingredient.orange = 0;
+		if (ingredient.blue > 0) {
+			print ("Explode!");
+		}
+
+		// Physical Interactions
+		if (ingredient.form == "leafy") {
+			ingredient.form = "pasty";
+		}
+
+		if (ingredient.form == "dried") {
+			ingredient.form = "powdery";
+		}
+
+		if (ingredient.form == "frozen") {
+			ingredient.form = "shaved";
+		}
+
+		if (ingredient.form == "fractured") {
+			ingredient.form = "powdery";
+		}
+
+		if (ingredient.form == "squishy") {
+			ingredient.form = "pasty";
+		}
+
+		if (ingredient.form == "brittle") {
+			ingredient.form = "powdery";
+		}
+	}
+}
