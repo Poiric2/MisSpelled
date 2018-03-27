5a05071d2a9344599bc2cfab80e6a65ce2475860
Pheonix <colecarpenter@gmail.com>
2018-03-26 21:58:29 -0400

    finished outline

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -21,7 +21,7 @@ public class PlayerController : MonoBehaviour {
     private Renderer rend;
 
     private Ray outlineRay;
-    public List<Outline> currOutlines;
+    private List<Outline> currOutlines;
     RaycastHit hit;
 
     // Use this for initialization
@@ -31,6 +31,7 @@ public class PlayerController : MonoBehaviour {
         inventory = GetComponent<Inventory>();
         shader1 = Shader.Find("Standard");
         shader2 = Shader.Find("Outlined/Regular");
+        currOutlines = new List<Outline>();
     }
 	
 	// Update is called once per frame
@@ -45,27 +46,13 @@ public class PlayerController : MonoBehaviour {
 
             outlineRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(outlineRay, out hit, interactDistance))
+            if ( Physics.Raycast(outlineRay, out hit, interactDistance))
             {
-                if (hit.transform.gameObject.GetComponent<Outline>() == null)
-                    currOutlines.AddRange(hit.transform.gameObject.GetComponentsInChildren<Outline>());
-
-                else
-                    currOutlines.Add(hit.transform.gameObject.GetComponent<Outline>());
-
-                foreach (Outline o in currOutlines)
-                {
-                    o.enabled = true;
-                }
+                PaintOutline();
             }
             else
             {
-                if (currOutlines.Count != 0)
-                    foreach(Outline o in currOutlines)
-                    {
-                        o.enabled = true;
-                    }
-                currOutlines.Clear();
+                ClearOutline();
             }
         }
         if (Input.GetKeyDown(KeyCode.L) && !canInteract)
@@ -111,6 +98,7 @@ public class PlayerController : MonoBehaviour {
         if (Physics.Raycast(ray, out hit, interactDistance)){
             if (hit.transform.tag == "interactable"){
                 SwapMovementState();
+                ClearOutline();
                 currStation = hit.transform.gameObject.GetComponent<Station>();
                 camLook.StartLerp(currStation.anchor, currStation.anchorRot, currStation.anchorCharRot);
                 currStation.StartMode(inventory);
@@ -118,8 +106,30 @@ public class PlayerController : MonoBehaviour {
             else if(hit.transform.tag == "pickup")
             {
                 inventory.AddNewItem(hit.transform.GetComponent<Pickup>().item);
+                ClearOutline();
                 Destroy(hit.transform.gameObject);
             }
         }
+    }
+
+    void PaintOutline()
+    {
+        currOutlines.AddRange(hit.transform.gameObject.GetComponentsInChildren<Outline>());
+
+        print(currOutlines.Count);
+        foreach (Outline o in currOutlines)
+        {
+            o.enabled = true;
+        }
+    }
+
+    void ClearOutline()
+    {
+        if (currOutlines.Count != 0)
+            foreach (Outline o in currOutlines)
+            {
+                o.enabled = false;
+            }
+        currOutlines.Clear();
     }
 }
