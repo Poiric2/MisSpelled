1860ce667a1f59fdb9c9d775120967315dfee036
Claira_Poirier <poiric2@rpi.edu>
2018-04-25 16:03:47 -0400

    Reduced script_objs to just Ingredient and Book

---
+++ Assets/Scripts/Items/Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Ingredient : ScriptableObject
+{
+    public string name;
+    public Sprite sprite;
+    public int count = 0;
+
+	public string form;
+	public string destruction = "pure";
+
+	public int red;
+	public int orange;
+	public int yellow;
+	public int green;
+	public int blue;
+	public int purple;
+}
